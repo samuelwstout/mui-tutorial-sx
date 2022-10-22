212b1e4dcbac221feff4d274a6c8cbb75f2beb81
Sam <samuelwstout@gmail.com>
2022-10-22 16:54:49 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
+import * as React from "react";
+import { Stack, Button } from "@mui/material";
+
 export default function App() {
+  const test = true;
   return (
-    <div>
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <Button
+        sx={[
+          {
+            width: {
+              xs: 100,
+              sm: 200,
+              md: 300,
+              lg: 400,
+              xl: 500,
+            },
+            m: 4,
+            border: 5,
+            borderColor: "secondary.main",
+            "&.MuiButton-root": {
+              height: "100px",
+            },
+          },
+          test && {
+            border: 10,
+            borderColor: "primary.main",
+          },
+        ]}
+        variant="contained"
+      >
+        Button 1
+      </Button>
+      <Button variant="outlined">Button 2</Button>
+    </>
   );
 }
